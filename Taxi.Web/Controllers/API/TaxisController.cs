68a22ef9da934c1428dcbd8ce39a90d7875a6229
dalvisa0@gmail.com <dalvisa0@gmail.com>
2020-06-10 13:01:32 -0500

    Video 14 completo, todo funcionando correctamente

--- Taxi.Web/Controllers/API/TaxisController.cs
+++ Taxi.Web/Controllers/API/TaxisController.cs
@@ -1,12 +1,9 @@
-﻿    using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 using Taxi.Web.Data;
 using Taxi.Web.Data.Entities;
+using Taxi.Web.Helpers;
 
 namespace Taxi.Web.Controllers.API
 {
@@ -15,10 +12,14 @@ namespace Taxi.Web.Controllers.API
     public class TaxisController : ControllerBase
     {
         private readonly DataContext _context;
+        private readonly IConverterHelper _converterHelper;
 
-        public TaxisController(DataContext context)
+        public TaxisController(
+            DataContext context,
+            IConverterHelper converterHelper)
         {
             _context = context;
+            _converterHelper = converterHelper;
         }
 
         [HttpGet("{plaque}")]
@@ -29,15 +30,22 @@ namespace Taxi.Web.Controllers.API
                 return BadRequest(ModelState);
             }
 
-            var taxiEntity = await _context.Taxis.FirstOrDefaultAsync(t =>t.Plaque == plaque);
+            TaxiEntity taxiEntity = await _context.Taxis
+               .Include(t => t.User) //Driver   
+               .Include(t => t.Trips)
+               .ThenInclude(t => t.TripDetails)
+               .Include(t => t.Trips)
+               .ThenInclude(t => t.User) //Passanger
+               .FirstOrDefaultAsync(t => t.Plaque == plaque);
+
 
             if (taxiEntity == null)
             {
                 return NotFound();
             }
 
-            return Ok(taxiEntity);
+            return Ok(_converterHelper.ToTaxiResponse(taxiEntity));
         }
 
-    }   
+    }
 }
