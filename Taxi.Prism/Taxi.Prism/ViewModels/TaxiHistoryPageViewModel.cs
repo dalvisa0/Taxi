bda029f3eed5e8691317519076e9baab23c4d800
Jeison Acosta <jeisonacostap1401@outlook.com>
2020-06-11 11:19:21 -0500

    Agregué la pagina trip detail para ver la decripcion de los viajes

--- Taxi.Prism/Taxi.Prism/ViewModels/TaxiHistoryPageViewModel.cs
+++ Taxi.Prism/Taxi.Prism/ViewModels/TaxiHistoryPageViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Prism.Commands;
 using Prism.Navigation;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using Taxi.Common.Models;
 using Taxi.Common.Services;
@@ -8,15 +10,23 @@ namespace Taxi.Prism.ViewModels
 {
     public class TaxiHistoryPageViewModel : ViewModelBase
     {
+        private readonly INavigationService _navigationService;
         private readonly IApiService _apiService;
         private TaxiResponse _taxi;
         private bool _isRunning;
+        private List<TripItemViewModel> _trips;
         private DelegateCommand _checkPlaqueCommand;
 
+        public List<TripItemViewModel> Trips
+        {
+            get => _trips;
+            set => SetProperty(ref _trips, value);
+        }
         public TaxiHistoryPageViewModel(
             INavigationService navigationService,
             IApiService apiService) : base(navigationService)
         {
+            _navigationService = navigationService;
             _apiService = apiService;
             Title = "Taxi History";
         }
@@ -82,6 +92,23 @@ namespace Taxi.Prism.ViewModels
             }
 
             Taxi = (TaxiResponse)response.Result;
+            Trips = Taxi.Trips.Where(t => t.Qualification != 0).Select(t => new TripItemViewModel(_navigationService)
+            {
+                EndDate = t.EndDate,
+                Id = t.Id,
+                Qualification = t.Qualification,
+                Remarks = t.Remarks,
+                Source = t.Source,
+                SourceLatitude = t.SourceLatitude,
+                SourceLongitude = t.SourceLongitude,
+                StartDate = t.StartDate,
+                Target = t.Target,
+                TargetLatitude = t.TargetLatitude,
+                TargetLongitude = t.TargetLongitude,
+                TripDetails = t.TripDetails,
+                User = t.User
+            }).OrderByDescending(t => t.StartDate).ToList();
+
         }
     }
 }
