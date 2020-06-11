4298350dbf823176c331b6c85fc1821d283a0880
Jeison Acosta <jeisonacostap1401@outlook.com>
2020-06-11 13:34:19 -0500

    Se agrego la opcion de cambiar contraseña y datos usuario

--- Taxi.Web/Helpers/IUserHelper.cs
+++ Taxi.Web/Helpers/IUserHelper.cs
@@ -22,5 +22,10 @@ namespace Taxi.Web.Helpers
 
         Task<UserEntity> AddUserAsync(AddUserViewModel model, string path);
 
+        Task<IdentityResult> ChangePasswordAsync(UserEntity user, string oldPassword, string newPassword);
+
+        Task<IdentityResult> UpdateUserAsync(UserEntity user);
+
+
     }
 }
