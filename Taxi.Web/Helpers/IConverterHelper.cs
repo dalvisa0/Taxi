7ff18639544056d400381554dbb19da75763eea9
dalvisa0@gmail.com <dalvisa0@gmail.com>
2020-06-12 14:19:22 -0500

    Añadidas vistas de confimacion y rechazo al grupo de usuario, sin probar :'v

--- Taxi.Web/Helpers/IConverterHelper.cs
+++ Taxi.Web/Helpers/IConverterHelper.cs
@@ -6,11 +6,15 @@ namespace Taxi.Web.Helpers
 {
     public interface IConverterHelper
     {
+        List<UserGroupDetailResponse> ToUserGroupResponse(List<UserGroupDetailEntity> users);
+
+        List<TripResponseWithTaxi> ToTripResponse(List<TripEntity> tripEntities);
+
         TaxiResponse ToTaxiResponse(TaxiEntity taxiEntity);
 
         TripResponse ToTripResponse(TripEntity tripEntity);
 
         UserResponse ToUserResponse(UserEntity user);
-        List<TripResponseWithTaxi> ToTripResponse(List<TripEntity> tripEntities);
+        
     }
 }
