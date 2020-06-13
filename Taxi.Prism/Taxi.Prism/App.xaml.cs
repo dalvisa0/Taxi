4156492327efcedfc004d0434a9862ab77a8618b
dalvisa0@gmail.com <dalvisa0@gmail.com>
2020-06-13 04:42:28 -0500

    Ya se puede cambiar la contraseña desde la app, la madre que me iré a dormir que ya casi amanece, quedé en el video 68, comienza en el 69 :3

--- Taxi.Prism/Taxi.Prism/App.xaml.cs
+++ Taxi.Prism/Taxi.Prism/App.xaml.cs
@@ -47,6 +47,7 @@ namespace Taxi.Prism
             containerRegistry.RegisterForNavigation<TripDetailPage, TripDetailPageViewModel>();
             containerRegistry.RegisterForNavigation<RegisterPage, RegisterPageViewModel>();
             containerRegistry.RegisterForNavigation<RememberPasswordPage, RememberPasswordPageViewModel>();
+            containerRegistry.RegisterForNavigation<ChangePasswordPage, ChangePasswordPageViewModel>();
         }
     }
 }
